2f1d5afeb79571bb2a151e0e57f1ececa4d04b66
James Casey <hanavi@gmail.com>
2018-04-21 02:23:58 -0500

    Added comments and a quick and dirty test program

---
+++ program_1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import logging
+def cpu_multiply(a,b,cpu):
+
+    # set memory references for eaze of use
+    tAddr = 18
+    m = tAddr + 20
+    r = 2
+
+    cpu._mem[0] = (cpu._LDI << 8) + 0b00000101
+    cpu._mem[1] = (cpu._JMP << 8) + tAddr
+    cpu._mem[2] = (cpu._LDA << 8) + 0b00000101
+    cpu._mem[3] = (cpu._OUT << 8) + 0b00000000
+    cpu._mem[4] = (cpu._HLT << 8) + 0b00000000
+    cpu._mem[5] = a
+    cpu._mem[6] = b
+
+    ####################################################################
+    # Test function: multiplication
+    ####################################################################
+
+
+    # Get the address of the first variable from reg_a and
+    # save it where we can get to it
+    cpu._mem[tAddr] = (cpu._STA << 8) + m
+
+    logging.debug(" -- {:016b}".format(cpu._PTA))
+    logging.debug(" -- {:016b}".format(cpu._PTA<<8))
+
+    # Get the value of the first variable and save it locally
+    cpu._mem[tAddr + 1] = (cpu._PTA << 8) + 0b00000000
+    cpu._mem[tAddr + 2] = (cpu._STA << 8) + m + 1
+
+    # Get the value of the second variable and save it locally
+    cpu._mem[tAddr + 3] = (cpu._LDI << 8) + 1
+    cpu._mem[tAddr + 4] = (cpu._ADD << 8) + m
+    cpu._mem[tAddr + 5] = (cpu._PTA << 8) + 0b00000000
+    cpu._mem[tAddr + 6] = (cpu._STA << 8) + m + 2
+
+    # Set up our "addition loop" for multiplying
+    cpu._mem[tAddr + 7] = (cpu._LDI << 8) + 0
+    cpu._mem[tAddr + 8] = (cpu._STA << 8) + m + 3
+    cpu._mem[tAddr + 9] = (cpu._LDI << 8) + 1
+    cpu._mem[tAddr + 10] = (cpu._STA << 8) + m + 4
+
+    # Do our loop work
+    cpu._mem[tAddr + 11] = (cpu._LDA << 8) + m + 3
+    cpu._mem[tAddr + 12] = (cpu._ADD << 8) + m + 1
+    cpu._mem[tAddr + 13] = (cpu._STA << 8) + m + 3
+
+    # Save the value to the original address
+    cpu._mem[tAddr + 14] = (cpu._ATP << 8) + m
+
+    # Decrement the multiplier
+    cpu._mem[tAddr + 15] = (cpu._LDA << 8) + m + 2
+    cpu._mem[tAddr + 16] = (cpu._SUB << 8) + m + 4
+    cpu._mem[tAddr + 17] = (cpu._STA << 8) + m + 2
+
+    # Check to see if we are done
+    cpu._mem[tAddr + 18] = (cpu._JEZ << 8) + r
+    cpu._mem[tAddr + 19] = (cpu._JMP << 8) + tAddr + 11
--- pycpu.py
+++ pycpu.py
@@ -6,14 +6,16 @@ import logging
 
 #logging.getLogger().setLevel(logging.DEBUG)
 
+from program_1 import cpu_multiply
+
 class pyCPU(object):
     def __init__(self):
 
         # Set up some variables
-        self._reg_a = 0
-        self._reg_b = 0
-        self._pc = 0
-        self._jf = 0
+        self._reg_a = 0 # Register A
+        self._reg_b = 0 # Register B
+        self._pc = 0    # Program Counter
+        self._jf = 0    # Jump flag (not implemented yet)
 
         # Current command list
         self._NOP = 0b00000000
@@ -27,7 +29,7 @@ class pyCPU(object):
         self._LDB = 0b00001000
         self._PTA = 0b00001001
         self._ATP = 0b00001010
-        # More to be added later?
+        # Gap here for more cmds
         self._OUT = 0b00001110
         self._HLT = 0b00001111
 
@@ -36,97 +38,124 @@ class pyCPU(object):
         for i in range(256):
             self._mem.append(0)
 
-    def nop(self,delay=0):
+    def _nop(self,delay=0):
+        """ NOP: Do nothing """
+
         if delay > 0:
             time.sleep(delay)
         self._pc += 1
 
-    def lda(self,addr):
+    def _lda(self,addr):
+        """ Load the value of addr to reg_a """
+
         logging.debug(" Loading 0x{:04x} to reg_a".format(addr))
         self._reg_a = self._mem[addr]
         self._pc += 1
 
-    def ldb(self,addr):
+    def _ldb(self,addr):
+        """ Load the value of addr to reg_b """
+
         self._reg_b = self._mem[addr]
         self._pc += 1
 
-    def ldi(self,value):
+    def _ldi(self,value):
+        """ Load the given value to reg_a """
+
         self._reg_a = value
         self._pc += 1
 
-    def add(self,addr):
+    def _add(self,addr):
+        """ Add the value in addr to the reg_a and place in reg_a """
+
         self._reg_b = self._mem[addr]
         self._reg_a = self._reg_a + self._reg_b
         self._pc += 1
 
-    def sub(self,addr):
+    def _sub(self,addr):
+        """ Subtract the value in addr from the value in reg_a and place in reg_a """
+
         self._reg_b = self._mem[addr]
         self._reg_a = self._reg_a - self._reg_b
         self._pc += 1
 
-    def jmp(self,addr):
+    def _jmp(self,addr):
+        """ Jump to addr """
+
         self._pc = addr
 
-    def pta(self):
+    def _pta(self):
+        """ Place the value from the address in addr into reg_a (pointer) """
+
         addr = self._reg_a
         self._reg_a = self._mem[addr]
         self._pc += 1
 
-    def atp(self, addr):
+    def _atp(self, addr):
+        """ Place the value into the address in addr from reg_a (pointer) """
+
         nAddr = self._mem[addr]
         self._mem[nAddr] = self._reg_a
         self._pc += 1
 
-    def jez(self,addr):
+    def _jez(self,addr):
+        """ Jump if the value in reg_a is 0 """
+
         #print("JEZ: {}".format(self._reg_a))
         if self._reg_a == 0:
             self._pc = addr
         else:
             self._pc += 1
 
-    def out(self):
+    def _out(self):
+        """ Print out the results in reg_a """
+
         print(self._reg_a)
         self._pc += 1
 
-    def hlt(self):
+    def _hlt(self):
+        """ Halt the program """
+
         sys.exit()
 
-    def sta(self,addr):
+    def _sta(self,addr):
+        """ Store the value in reg_a to addr """
+
         self._mem[addr] = self._reg_a
         self._pc += 1
 
     def run(self):
+        """ Run the program """
+
         while True:
-            #time.sleep(1)
-            print(self._pc)
-            r = self._mem[self._pc]
-            logging.debug(' {:016b}'.format(r))
+            r = self._mem[self._pc] # Get the command from memory
             cmd = r >> 8;
             arg = r & 0b11111111
+
+            logging.debug(' {:016b}'.format(r))
             logging.debug(" {}: {} {}".format(self._pc,cmd,arg))
 
             if cmd == self._NOP:
-                self.nop()
+                self._nop()
             elif cmd == self._LDA:
-                self.lda(arg)
+                self._lda(arg)
             elif cmd == self._ADD:
-                self.add(arg)
+                self._add(arg)
             elif cmd == self._SUB:
-                self.sub(arg)
+                self._sub(arg)
             elif cmd == self._LDI:
-                self.ldi(arg)
+                self._ldi(arg)
             elif cmd == self._JMP:
-                self.jmp(arg)
+                self._jmp(arg)
             elif cmd == self._JEZ:
-                self.jez(arg)
+                self._jez(arg)
             elif cmd == self._STA:
-                self.sta(arg)
+                self._sta(arg)
             elif cmd == self._LDB:
-                self.ldb(arg)
+                self._ldb(arg)
             elif cmd == self._PTA:
-                self.pta()
+                self._pta()
             elif cmd == self._ATP:
-                self.atp(arg)
+                self._atp(arg)
             elif cmd == 11:
                 pass
             elif cmd == 12:
@@ -134,9 +163,9 @@ class pyCPU(object):
             elif cmd == 13:
                 pass
             elif cmd == self._OUT:
-                self.out()
+                self._out()
             elif cmd == self._HLT:
-                self.hlt();
+                self._hlt();
             else:
                 print("Shouldn't be here...")
                 sys.exit()
@@ -148,8 +177,9 @@ class pyCPU(object):
 
 if __name__ == "__main__":
     testCPU = pyCPU()
-    testCPU.dumpMemory()
-    #testCPU.run()
+    #testCPU.dumpMemory()
+    cpu_multiply(7,83,testCPU)
+    testCPU.run()
 
 
 
